15341515802350444060293bae599e7f6c71a8f6
Jorge Villegas <jorge.villegasp@hotmail.com>
2022-10-29 12:28:29 -0500

    Moviendo los servicions a la carpeta extensiones

---
+++ TiendaWebApi/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,67 @@
+﻿using TiendaWebApi.Interfaces;
+using TiendaWebApi.Services;
+
+namespace TiendaWebApi.Extensions;
+public static class ApplicationServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+                services.AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy", builder =>
+                        builder.AllowAnyOrigin()   //WithOrigins("https://dominio.com")
+                        .AllowAnyMethod()          //WithMethods("GET","POST")
+                        .AllowAnyHeader());        //WithHeaders("accept","content-type")
+                });
+
+    public static void AddAplicacionServices(this IServiceCollection services)
+    {
+        //services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        //services.AddScoped<IProductoRepository, ProductoRepository>();
+        //services.AddScoped<IMarcaRepository, MarcaRepository>();
+        //services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+        services.AddScoped<UnitOfWorkInterface, UnitOfWorkService>();
+    }
+    /*
+    public static void ConfigureRateLimitiong(this IServiceCollection services)
+    {
+        services.AddMemoryCache();
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        services.AddInMemoryRateLimiting();
+
+        services.Configure<IpRateLimitOptions>(options =>
+        {
+            options.EnableEndpointRateLimiting = true;
+            options.StackBlockedRequests = false;
+            options.HttpStatusCode = 429;
+            options.RealIpHeader = "X-Real-IP";
+            options.GeneralRules = new List<RateLimitRule>
+                {
+                    new RateLimitRule
+                    {
+                        Endpoint ="*",
+                        Period = "10s",
+                        Limit =2
+                    }
+                };
+        });
+
+
+    }
+
+    public static void ConfigureApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            //options.ApiVersionReader = new QueryStringApiVersionReader("ver");
+            //options.ApiVersionReader = new HeaderApiVersionReader("X-Version");
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("ver"),
+                new HeaderApiVersionReader("X-Version"));
+            options.ReportApiVersions = true;
+        });
+    }
+
+    */
+}
--- TiendaWebApi/Program.cs
+++ TiendaWebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using TiendaWebApi.Extensions;
 using TiendaWebApi.Interfaces;
 using TiendaWebApi.Models;
 using TiendaWebApi.Services;
@@ -7,6 +8,9 @@ using TiendaWebApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+//builder.Services.AddAplicacionServices();
+//builder.Services.ConfigureApiVersioning();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,8 +24,7 @@ builder.Services.AddControllersWithViews()
                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
 
-builder.Services.AddScoped<ProductoInterface, ProductoService>();
-builder.Services.AddScoped<UnitOfWorkInterface, UnitOfWorkService>();
+
 
 // se construye el servidor de base de datos, eh indica la direccion donde esta ubicada la la cadena de conexion
 // para mas informacion vicite: https://learn.microsoft.com/es-es/ef/core/miscellaneous/connection-strings
